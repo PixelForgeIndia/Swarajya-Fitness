9accf0724797bf1cfede6659574004335fb18c43
Tejas Bhise <tejasbhise1013@gmail.com>
2026-05-22 00:23:53 +0530

    Move analytics outside router

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      <Analytics />
     </BrowserRouter>
+
+    <Analytics />
   </React.StrictMode>,
 )
